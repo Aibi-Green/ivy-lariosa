823bdf111fd8c05a75aaacde271dbc15c4be0af1
Ivy Lariosa <72445481+Aibi-Green@users.noreply.github.com>
2024-09-14 02:01:49 +0800

    second commit

---
+++ assets/scripts/index.js
@@ -0,0 +1,24 @@
+// const body = document.body
+// const div = document.createElement("div")
+
+// body.append(div)
+// div.innerText = "Hello World"
+// div.textContent = "Hello World"
+// div.innerHTML = "<strong>Hello World</strong>" <- this is not secure
+
+// const strong = document.createElement("strong")
+// strong.innerText = "Hello Web Dev Students!"
+// div.append(strong)
+
+// div.querySelector("div")
+// div.remove()
+
+// div.setAttributes("style", "colod: red;")
+// div.removeAttribute("style")
+
+
+// <span data-test="this is a test" data-longer-name="test">Hello</span>
+// console.log(spanHi.dataset)
+// prints
+// test: "this is a test"
+// longerName: "test"
